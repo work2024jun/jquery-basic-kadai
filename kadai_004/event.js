d472703bf3addafbe4ff7cab3e0b5a84b093181b
work2024jun <2024jobac@gmail.com>
2025-02-11 08:18:19 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(function(){ 
+   $(document).on('scroll',function(){
+    console.log('scrollイベントが発生しました');
+   });
+});
+
+$(window).on('load',function(){
+  console.log('loadイベントが発生しました');
+ });
